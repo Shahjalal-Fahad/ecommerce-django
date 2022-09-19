16373d420e22c89a504bfdf9d29d2668cd745718
Shahjalal-Fahad <shahjalalptw@gmail.com.com>
2022-09-19 09:39:45 +0600

    done

--- My_Ecom_Project/settings.py
+++ My_Ecom_Project/settings.py
@@ -74,11 +74,15 @@ WSGI_APPLICATION = 'My_Ecom_Project.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'pleasant',
+    'HOST': '127.0.0.1',
+    'PORT': '3306',
+    'USER': 'root',
+    'PASSWORD': '',
+}}
+
 
 
 # Password validation
